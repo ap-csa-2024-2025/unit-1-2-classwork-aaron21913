3a689d706ed6a9bd509e964a4cdbb4238302c8e2
aaronngo <atngo106@student.hbuhsd.edu>
2024-09-04 11:05:05 -0700

    donezo dab

--- problem2.java
+++ problem2.java
@@ -1,1 +1,15 @@
 // Create your class here; don't forget the main method
+public class problem2
+{
+    public static void main(String[] args)
+    {
+        String name1 = "ethan";
+        String name2 = "steven";
+        String name3 = "aaron";
+        String name4 = "phillip";
+        System.out.println(name4 + " " + name2 + " " + name3 + " " + name1);
+
+
+    }
+}
+    
--- problem3.java
+++ problem3.java
@@ -1,1 +1,10 @@
 // Create your class here; don't forget the main method
+public class problem3
+{
+    Public static void main(String[] args)
+    {
+      String name5 = "aaron";
+      String state = "California";
+      System.out.println("My name is " + name5 + "." "I live in " + state + ".");
+    }
+}
